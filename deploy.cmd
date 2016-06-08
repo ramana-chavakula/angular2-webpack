d7d77dff6a57ef60e49b22f55f3c4d60db771215
Rabi Kiran (Ravi) <yuvakiran2009@gmail.com>
2016-06-08 19:00:29 +0530

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -101,7 +101,6 @@ call :SelectNodeVersion
 IF EXIST "%DEPLOYMENT_TARGET%\package.json" (
   pushd "%DEPLOYMENT_TARGET%"
   call :ExecuteCmd !NPM_CMD! install
-  IF !ERRORLEVEL! NEQ 0 goto error
   popd
 )
 
